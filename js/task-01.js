2d7ad4beb54df35cac1db45ddf465366f67bb2d4
Oleksandr <114010794+Leximuzz@users.noreply.github.com>
2023-03-28 00:29:36 +0300

    with corrections

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,25 @@
+// const categoriesList = document.querySelector('#categories');
+// const items = categoriesList.querySelectorAll('.item');
+
+// console.log(`Number of categories: ${items.length}`);
+
+// items.forEach(item => {
+//   const heading = item.querySelector('h2').textContent;
+//   const elements = item.querySelectorAll('li').length;
+
+//   console.dir(`Category: ${heading}`);
+//   console.dir(`Elements: ${elements}`);
+// })
+
+
 const categoriesList = document.querySelector('#categories');
 const items = categoriesList.querySelectorAll('.item');
 
 console.log(`Number of categories: ${items.length}`);
 
 items.forEach(item => {
-  const heading = item.querySelector('h2').textContent;
-  const elements = item.querySelectorAll('li').length;
+  const heading = item.firstElementChild.textContent;
+  const elements = item.lastElementChild.children.length;
 
   console.dir(`Category: ${heading}`);
   console.dir(`Elements: ${elements}`);
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,11 @@
-const controlEl = document.querySelector('#font-size-control');
+//const controlEl = document.querySelector('#font-size-control');
+text.style.fontSize = `16px`; ///в початковий фонтсайз спана поставити початкове значення інпута
+const controlEl = document.querySelector('input#font-size-control');
+
 controlEl.addEventListener('input', onControl);
 function onControl(evt) {
     console.dir(evt.currentTarget.valueAsNumber);
   text.style.fontSize = `${evt.currentTarget.valueAsNumber}px`;
 }
+
+з врахуванням зауважень
